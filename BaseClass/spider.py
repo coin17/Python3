7f345a3b3decf725d6ec62e3bea477299f407aa5
Coin <xiao_coin@foxmail.com>
2017-03-10 16:35:08 +0800

    抓取修改为requests

--- BaseClass/spider.py
+++ BaseClass/spider.py
@@ -1,11 +1,14 @@
 # coding:UTF-8
 
 #encoding:UTF-8
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.douban.com/'
-webPage = urllib.request.urlopen(url)
-
+url = 'https://www.douban.com/'
+webPage = requests.get(url).text
 soup = BeautifulSoup(webPage,"html.parser")
 print(soup.title)
+
+
+test = "http://image.nmc.cn/static2/site/nmc/themes/basic/weather/white/day/1.png"
+print(test.split("/")[-1])
