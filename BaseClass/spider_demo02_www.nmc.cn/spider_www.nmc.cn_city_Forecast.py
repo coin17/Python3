bd06ef7f192faf397aca80dc799c06878c43412d
Coin <xiao_coin@foxmail.com>
2017-09-30 11:17:27 +0800

    添加超时设置

--- BaseClass/spider_demo02_www.nmc.cn/spider_www.nmc.cn_city_Forecast.py
+++ BaseClass/spider_demo02_www.nmc.cn/spider_www.nmc.cn_city_Forecast.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 def download_page(url):
     return requests.get(url,cookies=cookies, headers={
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
-    })
+    },timeout=60)
 
 icon_list = {}
 
