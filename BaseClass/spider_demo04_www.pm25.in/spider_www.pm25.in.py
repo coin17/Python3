5c367f16ffd5f928a9e2b8d96b5bdaffb13f40ca
Coin <xiao_coin@foxmail.com>
2017-10-10 10:54:29 +0800

    添加数据备份至mongo

--- BaseClass/spider_demo04_www.pm25.in/spider_www.pm25.in.py
+++ BaseClass/spider_demo04_www.pm25.in/spider_www.pm25.in.py
@@ -8,6 +8,9 @@ import time, os
 import requests
 import datetime
 from MSSql_SqlHelp import MSSQL 
+import pymongo
+# 数据备份至 mongo，需先安装 pymongo
+# pip install pymongo
 
 def download_page(url):
     return requests.get(url, headers={
@@ -17,6 +20,7 @@ def download_page(url):
 def parse_html_aqi(dis_aqi):
     print(len(dis_aqi))
     for index,x in enumerate(dis_aqi):
+        
         area = x["area"]
         position_name = x["position_name"]
         station_code = x["station_code"]
@@ -44,11 +48,18 @@ def parse_html_aqi(dis_aqi):
             so2_24h = x["so2_24h"]
             sql = "insert into Space0012A values ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s') " %(aqi,area,co,co_24h,no2,no2_24h,o3,o3_24h,o3_8h,o3_8h_24h,pm10,pm10_24h,pm2_5,pm2_5_24h,position_name,primary_pollutant,quality,so2,so2_24h,station_code,time_point)
             ms.ExecNonQuery(sql.encode('utf-8'))
+            # mongodb 数据备份
+            db.NationalControlAQI.insert_one(x).inserted_id
+
         print("完成解析数据index为："+str(index))
 
 
 #MS Sql Server 链接字符串
-ms = MSSQL(host=".",user="sa",pwd="sa",db="SmallIsBeautiful_2017-03-15")
+ms = MSSQL(host="172.16.12.35",user="sa",pwd="sa",db="SmallIsBeautiful_2017-03-15")
+
+#MongoDB 数据库链接
+client  = pymongo.MongoClient('172.16.21.232', 27017)
+db = client.OriginalData
 
 #每个 token 每小时最多调用 5 次
 token = ['7rMwJqMxrmuDRFsAxBqP','5j1znBVAsnSf5xQyNQyq','K6LgqdJKZP2R9Svedskd']
